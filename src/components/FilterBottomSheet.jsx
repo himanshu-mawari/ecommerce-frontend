a7c4214dcc708681675b8021597f2bdef67c2fa9
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-05-14 22:13:44 +0530

    feat: implement mobile order filter bottom sheet ui

---
+++ src/components/FilterBottomSheet.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import { X, Calendar, ChevronDown } from "lucide-react";
+
+const FilterBottomSheet = ({ isOpen, onClose }) => {
+  const [status, setStatus] = useState();
+  const [payment, setPayment] = useState("Paid");
+
+  return (
+    <>
+      <div
+        className={`fixed inset-0 bg-black/40 z-40 lg:hidden transition-opacity duration-300 ${
+          isOpen ? "opacity-100 visible" : "opacity-0 invisible"
+        }`}
+        onClick={() => onClose(false)}
+      />
+
+      <div
+        className={`
+          fixed bottom-0 left-0 right-0 z-50
+          h-[65vh] w-full bg-white rounded-t-[2.5rem] shadow-2xl
+          transition-transform duration-500 ease-[cubic-bezier(0.32,0.72,0,1)]
+          ${isOpen ? "translate-y-0" : "translate-y-full"}
+          lg:relative lg:h-auto lg:translate-y-0 lg:shadow-none lg:rounded-none lg:border-r lg:border-gray-100
+        `}
+      >
+        <div className="flex flex-col h-full ">
+          <div className="flex flex-col items-center pt-3 pb-2 border-b border-gray-300">
+            <div className="w-12 h-1.5 bg-gray-200 rounded-full mb-4 lg:hidden" />
+            <div className="flex justify-between items-center w-full px-6 pb-2">
+              <h1 className="text-xl font-bold text-gray-900">Filters</h1>
+              <button
+                className="p-1.5 hover:bg-gray-100 rounded-full text-gray-400 transition-colors"
+                onClick={() => onClose(false)}
+              >
+                <X className="size-6" />
+              </button>
+            </div>
+          </div>
+
+          <div className="flex-1 overflow-y-auto px-6 py-6 space-y-6">
+            <section>
+              <h3 className="text-md font-semibold tracking-wide mb-2">
+                Status
+              </h3>
+              <div>
+                {["Pending", "Completed", "Cancelled"].map((item) => (
+                  <label
+                    key={item}
+                    className={`flex items-center poppins gap-2 py-2 rounded-2xl transition-all cursor-pointer ${
+                      status === item
+                        ? "border-indigo-600 bg-indigo-50/30 "
+                        : "border-gray-100"
+                    }`}
+                  >
+                    <input
+                      type="radio"
+                      name="status"
+                      className="hidden"
+                      checked={status === item}
+                      onChange={() => setStatus(item)}
+                    />
+                    <div
+                      className={`size-5 rounded-full border-2 flex items-center justify-center ${
+                        status === item
+                          ? "border-indigo-600"
+                          : "border-gray-300"
+                      }`}
+                    >
+                      {status === item && (
+                        <div className="size-2.5 bg-indigo-600 rounded-full" />
+                      )}
+                    </div>
+                    <span className="font-light text-sm tracking-wider">
+                      {item}
+                    </span>
+                  </label>
+                ))}
+              </div>
+            </section>
+
+            <section>
+              <h3 className="text-sm font-semibold  tracking-wide  mb-2">
+                Payment
+              </h3>
+              <div className="flex gap-3">
+                {["Paid", "Unpaid"].map((item) => (
+                  <label
+                    key={item}
+                    className={`flex-1 text-sm text-center py-2 rounded-lg font-semibold transition-all cursor-pointer border-2 ${
+                      payment === item
+                        ? "bg-gray-900 border-gray-900 text-white shadow-md"
+                        : "bg-white border-gray-200"
+                    }`}
+                  >
+                    <input
+                      type="radio"
+                      className="hidden"
+                      checked={payment === item}
+                      onChange={() => setPayment(item)}
+                    />
+                    {item}
+                  </label>
+                ))}
+              </div>
+            </section>
+          </div>
+
+          <div className="p-2 border-t border-gray-100 grid grid-cols-2 gap-4 bg-white">
+            <button
+              onClick={() => {
+                setStatus("Pending");
+                setPayment("Paid");
+              }}
+              className="py-3  text-sm text-gray-500 font-medium hover:text-gray-700 active:scale-95 transition-all uppercase tracking-wide cursor-pointer"
+            >
+              Reset All
+            </button>
+            <button
+              onClick={() => onClose(false)}
+              className="py-3 text-sm bg-indigo-600 hover:bg-indigo-700 text-white font-medium rounded-lg shadow-lg shadow-indigo-100 active:scale-95 transition-all cursor-pointer"
+            >
+              Apply Filters
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FilterBottomSheet;
--- src/pages/AdminOrderPage.jsx
+++ src/pages/AdminOrderPage.jsx
@@ -1,52 +1,71 @@
-import React from "react";
+import { useState } from "react";
 import { CiClock1 } from "react-icons/ci";
-import { ImCancelCircle  } from "react-icons/im";
+import { ImCancelCircle } from "react-icons/im";
+import { MdOutlineCancel } from "react-icons/md";
+import FilterBottomSheet from "../components/FilterBottomSheet";
 
 const AdminOrderPage = () => {
-    const stats = [ 
-        {
-        label: "PENDING ORDERS",
-        value: "4",
-        icon: <CiClock1 />,
-        color: "text-orange-600",
-        bg: "bg-orange-50",
-      },
-        {
-        label: "CANCELLED ORDERS",
-        value: "4",
-        icon: <ImCancelCircle  />,
-        color: "text-red-600",
-        bg: "bg-red-50",
-      }
-    ]
+  const [isFilterOpen , setIsFilterOpen] = useState(false)
   return (
-    <div className="p-5">
+    <div className="p-5 inter">
       <div>
         <h1 className="text-3xl font-medium inter mb-0.5">Orders</h1>
         <p className="text-xs text-gray-500">
           Monitor, manage, and process customer purchases.
         </p>
       </div>
-      <div className="flex gap-3 mt-5">
-        {stats.map((item, index) => (
-          <div
-            key={index}
-            className="bg-white border border-gray-200  rounded-2xl transition-all duration-200 hover:shadow-md hover:-translate-y-1 flex flex-col p-3 "
-          >
-            <div className="flex">
-              <span className="text-[10px] font-bold text-gray-400 tracking-wider uppercase">
-                {item.label}
-              </span>
-              <div
-                className={`${item.bg} ${item.color} p-2 rounded-lg text-xl`}
-              >
-                {item.icon}
-              </div>
+      <div className="space-y-4">
+        <div className="flex gap-2 mt-5 w-full">
+          <div className="flex-1 flex items-center gap-3 p-2 bg-white border border-gray-200 rounded-xl transition-all duration-200 hover:shadow-md hover:-translate-y-1">
+            <div className="flex items-center justify-center bg-orange-50  w-9 h-9 rounded-lg text-2xl">
+              <CiClock1 className="w-4 h-4 text-orange-400" />
             </div>
-            <p className="text-xl font-medium text-gray-800">{item.value}</p>
+
+            <div className="flex flex-col">
+              <span className="text-[11px] font-medium text-gray-400 tracking-wide uppercase ">
+                Pending
+              </span>
+              <p className="text-base font-semibold text-gray-900">4</p>
+            </div>
           </div>
-        ))}
+          <div className="flex-1 flex items-center gap-3 px-4 py-2 bg-white border border-gray-200 rounded-xl transition-all duration-200 hover:shadow-md hover:-translate-y-1">
+            <div className="flex items-center justify-center bg-red-50  w-9 h-9 rounded-lg text-2xl">
+              <MdOutlineCancel className="w-4 h-4 text-red-400" />
+            </div>
+
+            <div className="flex flex-col">
+              <span className="text-[11px] font-medium text-gray-400 tracking-wide uppercase">
+                Cancelled
+              </span>
+              <p className="text-base font-semibold text-gray-900">4</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex justify-between gap-2">
+          <div className="relative flex-1">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                className="h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2.5"
+              >
+                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </div>
+            <input
+              type="text"
+              className="w-full rounded-xl border border-gray-200 py-2.5 pl-9 pr-4 focus:outline-none focus:ring-2 focus:ring-indigo-300 transition-all text-xs text-gray-600 placeholder:text-gray-400"
+              placeholder="Search by order Id and customer name.."
+            />
+          </div>
+          <button className="bg-indigo-700 text-white px-4 py-1 rounded-xl text-sm font-medium cursor-pointer hover:bg-indigo-800 active:scale-[0.98] transition-all duration-200" onClick={() => setIsFilterOpen(true)}>
+            Filters
+          </button>
+        </div>
       </div>
+      {isFilterOpen && <FilterBottomSheet isOpen={isFilterOpen} onClose={setIsFilterOpen} />}
     </div>
   );
 };
