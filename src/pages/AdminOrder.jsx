5b80a5a266174bc05628deaf9dd783714ca03286
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-05-20 16:10:21 +0530

    feat: implement responsive tablet layout for admin order page

--- src/pages/AdminOrder.jsx
+++ src/pages/AdminOrder.jsx
@@ -57,22 +57,24 @@ const AdminOrderPage = () => {
   };
 
   return (
-    <div className="px-4 py-6  inter">
+    <div className="px-4 py-6 md:px-12 inter">
       <div>
-        <h1 className="text-3xl font-bold tracking-tight mb-0.5">Orders</h1>
+        <h1 className="text-3xl md:text-4xl font-bold tracking-tight mb-0.5">
+          Orders
+        </h1>
         <p className="text-xs text-gray-500">
           Monitor, manage, and process customer purchases.
         </p>
       </div>
       <div className="space-y-4">
-        <div className="flex gap-2 mt-5 w-full">
+        <div className="flex gap-2 mt-5 w-full md:max-w-md md:gap-4">
           <div className="flex-1 flex items-center gap-3 p-2 bg-white border border-gray-200 rounded-xl transition-all duration-200 hover:shadow-md hover:-translate-y-1">
             <div className="flex items-center justify-center bg-orange-50  w-9 h-9 rounded-lg text-2xl">
               <CiClock1 className="w-4 h-4 text-orange-400" />
             </div>
 
             <div className="flex flex-col">
-              <span className="text-[11px] font-medium text-gray-400 tracking-wide uppercase ">
+              <span className="text-[11px] md:text-xs font-medium text-gray-400 tracking-wide uppercase ">
                 Pending
               </span>
               <p className="text-base font-semibold text-gray-900">4</p>
@@ -84,15 +86,15 @@ const AdminOrderPage = () => {
             </div>
 
             <div className="flex flex-col">
-              <span className="text-[11px] font-medium text-gray-400 tracking-wide uppercase">
+              <span className="text-[11px] md:text-xs font-medium text-gray-400 tracking-wide uppercase">
                 Cancelled
               </span>
               <p className="text-base font-semibold text-gray-900">4</p>
             </div>
           </div>
         </div>
-        <div className="w-full flex justify-between gap-2">
-          <div className="relative flex-1">
+        <div className="w-full flex justify-between md:justify-start gap-2 md:gap-4">
+          <div className="relative flex-1 md:flex-initial">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg
                 className="h-5 w-5 text-gray-400"
@@ -106,18 +108,22 @@ const AdminOrderPage = () => {
             </div>
             <input
               type="text"
-              className="w-full rounded-xl border border-gray-200 py-2.5 pl-9 pr-4 focus:outline-none focus:ring-2 focus:ring-indigo-300 transition-all text-xs text-gray-600 placeholder:text-gray-400"
+              className="w-full  md:w-lg rounded-xl border border-gray-200 py-2.5 md:py-4 pl-9 pr-4 focus:outline-none focus:ring-2 focus:ring-indigo-300 transition-all text-xs md:text-sm text-gray-600 placeholder:text-gray-400"
               placeholder="Search by order Id and customer name.."
             />
           </div>
-          <div className="flex items-center bg-indigo-700 rounded-xl px-4 py-1 gap-1 cursor-pointer hover:bg-indigo-800 active:scale-[0.98] transition-all duration-200">
+
+          <div
+            className="flex items-center bg-indigo-700 rounded-xl px-4 py-1 md:py-2 md:h-auto gap-1 cursor-pointer hover:bg-indigo-800 active:scale-[0.98] transition-all duration-200"
+            onClick={() => setIsFilterOpen(true)}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
-              class="size-5 text-white"
+              className="size-5 md:size-6 text-white"
             >
               <path
                 strokeLinecap="round"
@@ -126,10 +132,7 @@ const AdminOrderPage = () => {
               />
             </svg>
 
-            <button
-              className=" text-white text-sm font-medium cursor-pointer"
-              onClick={() => setIsFilterOpen(true)}
-            >
+            <button className="text-white text-sm md:text-lg md:w-24 font-medium cursor-pointer">
               Filters
             </button>
           </div>
@@ -140,10 +143,10 @@ const AdminOrderPage = () => {
               <div className="bg-white border border-gray-200 p-4 rounded-xl shadow-[0_2px_10px_-3px_rgba(0,0,0,0.07)] active:bg-gray-50 transition-colors cursor-pointer hover:border-gray-400 ">
                 <div className="flex justify-between items-start mb-1">
                   <div className="flex flex-col">
-                    <h2 className="text-[14px] font-bold text-gray-900 tracking-tight">
+                    <h2 className="text-[14px] md:text-lg font-bold text-gray-900 tracking-tight">
                       ORD-{order.id}
                     </h2>
-                    <div className="flex items-center gap-1.5 text-[12px] text-gray-500 mt-0.5">
+                    <div className="flex items-center gap-1.5 text-[12px] md:text-sm text-gray-500 mt-0.5">
                       <span className="font-medium text-gray-700">
                         {order.customer}
                       </span>
@@ -152,20 +155,20 @@ const AdminOrderPage = () => {
                     </div>
                   </div>
 
-                  <p className="text-[15px] font-bold text-gray-900">
+                  <p className="text-[15px] md:text-xl font-bold text-gray-900">
                     ₹{order.amount}
                   </p>
                 </div>
 
                 <div className="mt-3 mb-4">
-                  <p className="text-[13px] text-gray-600 font-normal line-clamp-1 pl-2">
+                  <p className="text-[13px] md:text-lg text-gray-600 font-normal line-clamp-1 pl-2">
                     {order.itemsPreview}
                   </p>
                 </div>
 
                 <div className="flex justify-end">
                   <span
-                    className={`px-3 py-1 rounded-md text-[10px] font-extrabold uppercase tracking-normal ${
+                    className={`px-3 py-1 rounded-md text-[10px] md:text-sm font-extrabold uppercase tracking-normal ${
                       statusStyles[order.status.toLowerCase()]
                     }`}
                   >
