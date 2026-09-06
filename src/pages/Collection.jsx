10b35ee39dbbfca9686968ec2904e6951a891285
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-09-06 22:07:47 +0530

    feat: add error state on collection page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -3,7 +3,9 @@ import { useParams, useSearchParams } from "react-router-dom";
 import { useGetProductsQuery } from "../services/productService.js";
 import CollectionSkeleton from "../components/CollectionSkeleton.jsx";
 import useDebounce from "../helpers/useDebounce.js";
-import EmptySearchState from "../components/EmptySearchState.jsx"; 
+import EmptySearchState from "../components/EmptySearchState.jsx";
+import ErrorState from "../components/ErrorState";
+import useErrorHandler from "../hooks/useErrorHandler";
 
 const Collection = () => {
   const { category } = useParams();
@@ -27,31 +29,31 @@ const Collection = () => {
     filters.collectionType = category;
   }
 
-  const { data, isLoading, error } = useGetProductsQuery(filters);
+  const { data, isLoading, isError, error, refetch, isFetching } =
+    useGetProductsQuery(filters);
 
   const products = data?.data || {};
-
-  if (isLoading)
-    return (
-      <div>
-        <CollectionSkeleton />
-      </div>
-    );
-  if (error) return <p>Error...</p>;
 
   const capitalizeFirstAlphabet = (str) => {
     return str[0].toUpperCase() + str.slice(1);
   };
 
-  if (!products.length) {
-    return (
-      <div className="my-36 md:my-44">
-        <EmptySearchState value={debounceSearch} />
-      </div>
-    );
-  }
+  const { message, showRetry } = useErrorHandler(error, "Products");
 
-  return (
+  return isLoading ? (
+    <CollectionSkeleton />
+  ) : !isError ? (
+    <ErrorState
+      message={message}
+      onRetry={refetch}
+      showRetry={showRetry}
+      isRetrying={isFetching}
+    />
+  ) : !products.length ? (
+    <div className="my-36 md:my-40">
+      <EmptySearchState value={debounceSearch} />
+    </div>
+  ) : (
     <div className="px-4 md:px-8 lg:px-14 xl:px-24 border-t border-gray-300">
       <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-semibold pt-8 pb-8">
         {category === "shop-all"
