6b2fcc794f3e453227b34e4ab2c8ca9fa39ca2c2
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-09-09 23:22:38 +0530

    fix: add Link to review now button with navigation

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -282,7 +282,7 @@ const AdminDashboard = () => {
                       );
                     })}
                     <Link
-                    to="/admin/products"
+                      to="/admin/products"
                       className="flex justify-center w-full"
                     >
                       <button className="w-full lg:w-60 py-3 lg:py-2  bg-indigo-700 hover:bg-indigo-800 transition-all text-white rounded-xl font-medium shadow-sm active:scale-[0.98] cursor-pointer">
@@ -319,11 +319,14 @@ const AdminDashboard = () => {
                         waiting to be processed.
                       </p>
                     </div>
-                    <div className="flex justify-center w-full">
+                    <Link
+                      to="/admin/orders?order_status=pending"
+                      className="flex justify-center w-full"
+                    >
                       <button className="w-full lg:w-60 py-3 lg:py-2  bg-indigo-700 hover:bg-indigo-800 transition-all text-white rounded-xl font-medium shadow-sm active:scale-[0.98] cursor-pointer">
                         Review now
                       </button>
-                    </div>
+                    </Link>
                   </div>
                 )}
               </div>
