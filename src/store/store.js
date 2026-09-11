eebf95a3ef045e047ab66784e38a46b23be578e7
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-09-11 11:10:14 +0530

    chore: remove cartSlice, orderSlice, toastSlice, userSlice from store

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import addressSlice from "./addressSlice";
-import orderSlice from "./orderSlice";
-import cartSlice from "./cartSlice";
-import userSlice from "./userSlice.js"
-import toastSlice from "./toastSlice";
 
-import { baseApi } from "../services/baseApi.js"; 
+import { baseApi } from "../services/baseApi.js";
 
 const store = configureStore({
   reducer: {
-    user:userSlice,
-    cart: cartSlice,
     address: addressSlice,
-    order: orderSlice,
-    toast: toastSlice,
 
     [baseApi.reducerPath]: baseApi.reducer,
   },
